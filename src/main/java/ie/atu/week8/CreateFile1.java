18908a59a8a2272bb100ed1bcb53af104ca5ae4a
alan-hynes <G00400498@atu.ie>
2022-11-15 13:32:24 +0000

    CreateFile1 v1

---
+++ src/main/java/ie/atu/week8/CreateFile1.java
@@ -0,0 +1,16 @@
+package ie.atu.week8;
+
+import java.io.*;
+
+public class CreateFile1 {
+    public static void main(String[] args) {
+
+        {
+            File myFile = new File("MyFile.txt");
+            System.out.println("My file is located at" + myFile.getAbsolutePath());
+
+
+        }
+
+    }
+}
